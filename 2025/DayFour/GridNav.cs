69a94912304ddeb9f34904bff33968b9a2f30f91
Zachary Zak <zachary.zak@sanfordhealth.org>
2025-12-05 10:54:23 -0600

    2025/Day Four solutions for part 1 and 2

---
+++ 2025/DayFour/GridNav.cs
@@ -0,0 +1,135 @@
+﻿namespace DayFour;
+
+internal static class GridNav
+{
+    public static int CountSurroundingPapers(this char[,] grid, int x, int y)
+    {
+        int surroundingPaperCount = 0;
+
+        // if 5,5
+        // x = row
+        // y = column
+
+        // check up
+        if (IsPaper(grid, x - 1, y))
+            surroundingPaperCount++;
+
+        // check up/right
+        if (IsPaper(grid, x - 1, y + 1))
+            surroundingPaperCount++;
+
+        // check right
+        if (IsPaper(grid, x, y + 1))
+            surroundingPaperCount++;
+
+        // check right/down
+        if (IsPaper(grid, x + 1, y + 1))
+            surroundingPaperCount++;
+
+        // check down
+        if (IsPaper(grid, x + 1, y))
+            surroundingPaperCount++;
+
+        // check down/left
+        if (IsPaper(grid, x + 1, y - 1))
+            surroundingPaperCount++;
+
+        // check left
+        if (IsPaper(grid, x, y - 1))
+            surroundingPaperCount++;
+
+        // check up/left
+        if (IsPaper(grid, x - 1, y - 1))
+            surroundingPaperCount++;
+
+        return surroundingPaperCount;
+    }
+
+    public static char[,] ScanGrid(this char[,] grid)
+    {
+        var updatedGrid = grid;
+
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                if (grid[i, j] == '@')
+                {
+                    var paperCount = grid.CountSurroundingPapers(i, j);
+
+                    if (paperCount < 4)
+                    {
+                        updatedGrid[i, j] = 'X';
+                    }
+                }
+            }
+        }
+
+        return updatedGrid;
+    }
+
+    public static int CountValidPapers(this char[,] grid)
+    {
+        int totalValidPapers = 0;
+
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                if (grid[i, j] == 'X')
+                {
+                    totalValidPapers++;
+                }
+            }
+        }
+
+        return totalValidPapers;
+    }
+
+
+    public static char[,] CleanGrid(this char[,] grid)
+    {
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                if (grid[i, j] == 'X')
+                {
+                    grid[i, j] = '.';
+                }
+            }
+        }
+
+        return grid;
+    }
+
+    public static void Print(this char[,] grid)
+    {
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                Console.Write(grid[i, j]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    private static bool IsPaper(char[,] grid, int x, int y)
+    {
+        if (x < 0 || y < 0)
+        {
+            return false;
+        }
+
+        if (x >= grid.GetLength(0) || y >= grid.GetLength(1))
+        {
+            return false;
+        }
+
+        return grid[x, y] == '@' || grid[x,y] == 'X';
+    }
+
+
+}
---
+++ 2025/DayFour/Program.cs
@@ -0,0 +1,45 @@
+﻿//READ THE INPUT FILE
+using DayFour;
+
+FileInfo inputFile = new(args[0]);
+
+IEnumerable<string> fileLines = [];
+using (var reader = new StreamReader(inputFile.FullName))
+{
+    while (!reader.EndOfStream)
+    {
+        fileLines = fileLines.Append(reader.ReadLine());
+    }
+}
+
+char[,] grid = new char[fileLines.Count(), fileLines.First().ToArray().Length];
+char[,] updatedGrid = new char[fileLines.Count(), fileLines.First().ToArray().Length];
+
+int totalValidPapers = 0;
+
+for (int i = 0; i < fileLines.Count(); i++)
+{
+    var lineAsArray = fileLines.ElementAt(i).ToArray();
+    for (var j = 0; j < lineAsArray.Length; j++)
+    {
+        grid.SetValue(lineAsArray[j], i, j);
+    }
+}
+
+//grid.Print();
+
+int paperCount = 0;
+do
+{
+    updatedGrid = grid.ScanGrid();
+    //Console.WriteLine();
+    //updatedGrid.Print();
+    paperCount = updatedGrid.CountValidPapers();
+    totalValidPapers += paperCount;
+    grid = updatedGrid.CleanGrid();
+    //Console.WriteLine();
+    //grid.Print();
+}
+while (paperCount > 0);
+
+Console.WriteLine($"Total papers to pull: {totalValidPapers}");
